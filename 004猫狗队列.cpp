c7658b8b2bc3ced55cfc85f612c107f04b6c478f
GK from home <304488689@qq.com>
2020-08-05 22:35:53 +0800

    004 is half completed

--- 004猫狗队列.cpp
+++ 004猫狗队列.cpp
@@ -7,12 +7,102 @@
 using namespace std;
 
 /**
- * @author
- * @date
- * @function
- *
+ * @author GK
+ * @date 2020/8/5
+ * @function 根据猫狗类，实现一种猫狗队列的结构，要求如下：
+ * 			1. 用户可以调用add方法将cat类或dog类的实例放入队列中；
+ *   		2. pollAll:将队列中所有的实例弹出FIFO	pollDog:将队列中dog类实例弹出FIFO  pollCat:将队列中cat类实例弹出FIFO
+ *   		3. isEmpty:检查队列中是否还有dog或cat实例	isEmptyDog:检查队列中是否还有dog或cat实例  itEmptyCat:检查队列中是否还有dog或cat实例
 **/
 
+class Pet{
+	string type;
+
+public:
+	Pet(string t):type(t){}
+	Pet(const Pet& p){
+		this->type = p.getPetType();
+	}
+	string getPetType() const{
+		return this->type;
+	}
+};
+
+class Dog: public Pet{
+
+public:
+	Dog():Pet("dog"){}
+};
+
+class Cat: public Pet{
+
+public:
+	Cat():Pet("cat"){}
+};
+
+class PetEnterQueue{
+	Pet pet;
+	unsigned long long count;
+
+public:
+	PetEnterQueue(Pet p, unsigned long long c):pet(p),count(c){}
+
+	Pet& getPet(){
+        return this->pet;
+	}
+
+	unsigned long long getCount(){
+		return this->count;
+	}
+
+	string getEnterPetType(){
+		return this->pet.getPetType();
+	}
+};
+
+class CDQueue{
+	queue<PetEnterQueue> dogq;
+	queue<PetEnterQueue> catq;
+	unsigned long long count;
+
+public:
+	CDQueue():count(0){}
+    void add(Pet&);
+    Pet& pollAll();
+    Dog& pollDog();
+    Cat& pollCat();
+    bool isEmpty();
+    bool isEmptyDog();
+    bool isEmptyCat();
+};
+
+void CDQueue::add(Pet& pet){
+	if(pet.getPetType().compare("dog") == 0)
+		dogq.push(PetEnterQueue(pet,count++));
+    else if(pet.getPetType().compare("cat") == 0)
+		catq.push(PetEnterQueue(pet,count++));
+	else
+		cout << "Neither cat or dog..." << endl;
+}
+
+Pet& CDQueue::pollAll(){
+	if(dogq.empty() && catq.empty())
+		return nullptr;
+	if(dogq.empty() || dogq.front().getCount() > catq.front().getCount()){
+		Pet& result = catq.front();
+		catq.pop();
+	}else{
+		Pet& result = dogq.front();
+		dogq.pop();
+	}
+		return result;
+}
+
+Dog& CDQueue::pollDog(){}
+Cat& CDQueue::pollCat(){}
+bool CDQueue::isEmpty(){}
+bool CDQueue::isEmptyDog(){}
+bool CDQueue::isEmptyCat(){}
 
 int main(void){
 
